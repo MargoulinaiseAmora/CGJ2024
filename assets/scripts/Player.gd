b8bb4013080f6cb2247c923641d27214aaf84c8a
Atzelord <94599320+Atzelord@users.noreply.github.com>
2024-01-23 21:30:31 +0100

    switch body spirit

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -1,40 +1,12 @@
-extends KinematicBody2D
+extends Node2D
 
-const UP = Vector2(0, -1)
-export var GRAVITY = 20
-export var SPEED = 300
-export var JUMP_HEIGHT = -500
-var jumped = false
+export var meditating = false;
 
-var motion = Vector2()
-
-var hit_the_ground = false
-
-func _physics_process(delta):
-	motion.y += GRAVITY
-	
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-	elif Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-	else:
-		motion.x = 0
-		
-	motion = move_and_slide(motion, UP, false)
-	
-	if is_on_floor():
-		$AnimatedSprite.scale = lerp($AnimatedSprite.scale, Vector2(1,1), 0.25)
-		if jumped:
-			$AnimatedSprite.scale = Vector2(1.35, 0.65)
-			jumped = false
-		motion.y = 0	
-		if Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("ui_select"):
-			jumped = true
-			motion.y = JUMP_HEIGHT
-	else:
-		if jumped:
-			$AnimatedSprite.scale = lerp($AnimatedSprite.scale, Vector2(0.75, 1.25), 0.25)
-		motion.y += GRAVITY
-
-	
-	
+func _process(delta):
+	$Spirit.visible = meditating
+	if !$Body.is_on_floor():
+		meditating = false
+	elif Input.is_action_just_pressed("ui_select"):
+		meditating = !meditating
+		if meditating:
+			$Spirit.position = $Body.position
--- assets/scripts/Spirit.gd
+++ assets/scripts/Spirit.gd
@@ -4,7 +4,6 @@ export var speed = 400
 export var velocity = Vector2()
 
 func get_input():
-	# Detect up/down/left/right keystate and only move when pressed.
 	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
@@ -12,10 +11,12 @@ func get_input():
 		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
-	if Input.is_action_pressed('ui_up') || Input.is_action_pressed('ui_select'):
+	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	if get_parent().meditating:
+		get_input()
+		move_and_collide(velocity * delta)
+		
