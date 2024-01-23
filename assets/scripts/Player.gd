f2402bf4de5648f11d04a54746be2e5badb855b9
Ynox <sonicdimensionproject@gmail.com>
2024-01-24 00:25:37 +0100

    fix joueur oeeeeeee

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -1,13 +1,49 @@
-extends Node2D
+extends KinematicBody2D
 
-export var meditating = false;
+const UP = Vector2(0, -1)
+export var GRAVITY = 20
+export var SPEED = 250
+export var JUMP_HEIGHT = -500
+var jumped = false
 
-func _process(delta):
-	$Spirit.visible = meditating
-	if !$Body.is_on_floor():
-		meditating = false
-	elif Input.is_action_just_pressed("ui_select"):
-		meditating = !meditating
-		if meditating:
-			$Spirit.position = $Body.position
-			$Spirit.velocity = Vector2()
+var motion = Vector2()
+
+var hit_the_ground = false
+
+func _ready():
+	$AnimationTree.get("parameters/playback").start("Idle")
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$AnimatedSprite.flip_h = true;
+		$AnimationTree.get("parameters/playback").travel("Walk")
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$AnimatedSprite.flip_h = false;
+		$AnimationTree.get("parameters/playback").travel("Walk")
+	else:
+		motion.x = 0
+		$AnimationTree.get("parameters/playback").travel("Idle")
+
+		
+	motion = move_and_slide(motion, UP, false)
+	
+	if is_on_floor():
+		if jumped:
+			$AnimationTree.get("parameters/playback").travel("Land")
+			jumped = false
+		motion.y = 0	
+		if Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("ui_select"):
+			jumped = true
+			motion.y = JUMP_HEIGHT
+	else:
+		if jumped:
+			$AnimationTree.get("parameters/playback").travel("Jump")
+			
+		motion.y += GRAVITY
+
+	
+	
--- assets/scripts/Spirit.gd
+++ assets/scripts/Spirit.gd
@@ -1,28 +1,21 @@
 extends KinematicBody2D
 
-export var speed = 500
-export var acceleration = 0.1
-export var deceleration = 0.04
+export var speed = 400
 export var velocity = Vector2()
 
 func get_input():
-	var target_velocity = Vector2()
+	# Detect up/down/left/right keystate and only move when pressed.
+	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
-		target_velocity.x += 1
+		velocity.x += 1
 	if Input.is_action_pressed('ui_left'):
-		target_velocity.x -= 1
+		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
-		target_velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		target_velocity.y -= 1
-	target_velocity = target_velocity.normalized() * speed
-	
-	if target_velocity == Vector2.ZERO:
-		velocity = velocity.linear_interpolate(Vector2.ZERO, deceleration)
-	else:
-		velocity = velocity.linear_interpolate(target_velocity, acceleration)
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up') || Input.is_action_pressed('ui_select'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
-	if get_parent().meditating:
-		get_input()
-		move_and_collide(velocity * delta)
+	get_input()
+	move_and_collide(velocity * delta)
