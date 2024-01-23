b51adbddbf258ff8dd3b5a131e838dd1ff4755b9
Ynox <sonicdimensionproject@gmail.com>
2024-01-23 19:45:23 +0100

    Animation joueur amélioré

--- assets/scripts/LevelSelectionManager.cs
+++ assets/scripts/LevelSelectionManager.cs
@@ -6,14 +6,10 @@ public class LevelSelectionManager : Control
     PackedScene[] scenes;
 
     // Called when the node enters the scene tree for the first time.
-    private void _Ready()
+    private void LaunchLevel(int index)
     {
-        
+        var levelInstance = scenes[index].Instance();
+        GetTree().Root.AddChild(levelInstance);
+        this.QueueFree();
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
